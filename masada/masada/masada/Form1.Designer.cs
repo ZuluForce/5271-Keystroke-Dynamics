90db7c0ef7dcd67e58ab31026f0b187aa6669e20
unknown <Ethan@Ethan-PC.(none)>
2012-10-22 23:50:31 -0500

    started on the program to break, called masada. sorry about all the visual studio files...

---
+++ masada/masada/masada/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace masada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.logingButton = new System.Windows.Forms.Button();
+            this.adduserButton = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(237, 45);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(133, 20);
+            this.usernameTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(237, 81);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(133, 20);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // logingButton
+            // 
+            this.logingButton.Location = new System.Drawing.Point(184, 136);
+            this.logingButton.Name = "logingButton";
+            this.logingButton.Size = new System.Drawing.Size(85, 23);
+            this.logingButton.TabIndex = 2;
+            this.logingButton.Text = "Login";
+            this.logingButton.UseVisualStyleBackColor = true;
+            this.logingButton.Click += new System.EventHandler(this.logingButton_Click);
+            // 
+            // adduserButton
+            // 
+            this.adduserButton.Location = new System.Drawing.Point(285, 136);
+            this.adduserButton.Name = "adduserButton";
+            this.adduserButton.Size = new System.Drawing.Size(85, 23);
+            this.adduserButton.TabIndex = 3;
+            this.adduserButton.Text = "Add User";
+            this.adduserButton.UseVisualStyleBackColor = true;
+            this.adduserButton.Click += new System.EventHandler(this.adduserButton_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(181, 81);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(181, 45);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.usernameLabel.TabIndex = 5;
+            this.usernameLabel.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(543, 242);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.adduserButton);
+            this.Controls.Add(this.logingButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button logingButton;
+        private System.Windows.Forms.Button adduserButton;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label usernameLabel;
+
+
+    }
+}
+
---
+++ masada/masada/masada/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace masada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void logingButton_Click(object sender, EventArgs e)
+        {
+            if (checkUser(usernameTextBox.Text, passwordTextBox.Text))
+            {
+                MessageBox.Show("Logged In", "Success", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("Failed to Login", "Success", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private bool checkUser(string _username, string _password)
+        {
+            string[] lines = openLoginFile();
+
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            foreach (string line in lines)
+            {
+                if (line.Equals(""))
+                {
+                    System.Diagnostics.Debug.WriteLine("Found an empty value...continuing");
+                }
+                else
+                {
+                    string tempLine = line.Trim();
+                    string[] tempLines = tempLine.Split(':');
+                    if (tempLines.Length == 2)
+                    {
+                        if (tempLines[0].Equals(username) && tempLines[1].Equals(password))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool checkUserByUsername(string _username)
+        {
+            string[] lines = openLoginFile();
+
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            foreach (string line in lines)
+            {
+                if (line.Equals(""))
+                {
+                    System.Diagnostics.Debug.WriteLine("Found an empty value...continuing");
+                }
+                else
+                {
+                    string tempLine = line.Trim();
+                    string[] tempLines = tempLine.Split(':');
+                    if (tempLines.Length == 2)
+                    {
+                        if (tempLines[0].Equals(username))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void adduserButton_Click(object sender, EventArgs e)
+        {
+            if (!checkUserByUsername(usernameTextBox.Text))
+            {
+                writeToLoginFile(usernameTextBox.Text + ":" + passwordTextBox.Text);
+                MessageBox.Show("Successfully added " + usernameTextBox.Text + " to the list of users! Proceed to login.", "Success", MessageBoxButtons.OK);
+                usernameTextBox.Text = "";
+                passwordTextBox.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("User " + usernameTextBox.Text + " appears to already exist", "Failure", MessageBoxButtons.OK);
+                usernameTextBox.Text = "";
+                passwordTextBox.Text = "";
+            }
+        }
+
+        private string[] openLoginFile()
+        {
+            string text = System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + "\\login.txt");
+            string[] lines = text.Split('\r');
+            return lines;
+        }
+
+        private void writeToLoginFile(string _write)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\login.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(_write);
+            }
+        }
+ 
+    }
+}
