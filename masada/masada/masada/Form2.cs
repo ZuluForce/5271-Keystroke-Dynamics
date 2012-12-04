516481428d268e05055e87c6ca6456da58f28e27
unknown <Ethan@Ethan-PC.(none)>
2012-12-04 17:42:52 -0600

    added slider bar and dynamic paragraph text

--- masada/masada/masada/Form2.cs
+++ masada/masada/masada/Form2.cs
@@ -71,11 +71,18 @@ namespace masada
 
         // this is currently in seconds, probably will change to some other format when needed (2000.0 = 2 seconds)
         double maxFilterTime = 2.0;
+        double stdForDistance = 1.0;
 
         public Form2()
         {
             InitializeComponent();
+            stdBox.Text = stdForDistance.ToString();
             typingBoxText.Text = "Feel free to type, it will not be saved...";
+            paragraphText.Text = "So then, his armour being furbished, his morion turned into a helmet, his hack christened, and he himself confirmed, he came to the conclusion that nothing more was needed now but to look out for a lady to be in love with; for a knight-errant without love was like a tree without leaves or fruit, or a body without a soul. As he said to himself, \"If, for my sins, or by my good fortune, I come across some giant hereabouts, a common occurrence with knights-errant, and overthrow him in one onslaught, or cleave him asunder to the waist, or, in short, vanquish and subdue him, will it not be well to have some one I may send him to as a present, that he may come in and fall on his knees before my sweet lady, and in a humble, submissive voice say, 'I am the giant Caraculiambro, lord of the island of Malindrania, vanquished in single combat by the never sufficiently extolled knight Don Quixote of La Mancha, who has commanded me to present myself before your Grace, that your Highness dispose of me at your pleasure'?\" Oh, how our good gentleman enjoyed the delivery of this speech, especially when he had thought of some one to call his Lady!";
+            trackBar1.Value = (int) stdForDistance;
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 20;
+            trackBar1.TickFrequency = 1;
             timer1.Start();
         }
 
@@ -331,5 +338,22 @@ namespace masada
         {
             ticks++;
         }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.stdBox.Text = this.getStdDev().ToString();
+        }
+
+        private double getStdDev()
+        {
+            int std = this.trackBar1.Value;
+
+            double result = 0;
+
+            result = std * 0.25;
+
+            return result;
+        }
+
     }
 }
